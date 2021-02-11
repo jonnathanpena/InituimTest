e49cf274a334eb368182651ca183cf1eb4455524
Jonnathan Peña <razaisger@hotmail.com>
2021-02-10 20:29:11 -0500

    before compodoc

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ClockComponent } from './clock/clock.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /* Externals Libs */
@@ -14,7 +15,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ClockComponent
   ],
   imports: [
     FlexLayoutModule,
