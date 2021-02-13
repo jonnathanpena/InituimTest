3bbb7de1cda781d604c589fababcb451f1be4ddc
Jonnathan Peña <razaisger@hotmail.com>
2021-02-13 13:01:55 -0500

    consulta lista y procesa cola

--- Controllers/QueueController.cs
+++ Controllers/QueueController.cs
@@ -28,9 +28,10 @@ namespace InitiumTest.Controllers
             Contiene las funciones principales para registrar/editar un cliente y asignarle un turno
         */
         [HttpGet("[action]")]
-        public IActionResult Init()
+        public IActionResult All()
         {
-            return Json("Hello");
+            queueFunctions = new QueueFunctions(_db);
+            return Json(queueFunctions.queueList());
         }
 
         /*
@@ -40,11 +41,13 @@ namespace InitiumTest.Controllers
         [HttpPost("[action]")]
         public IActionResult GetTurn([FromBody]Client client)
         {
+            // Cargamos las variables que nos servirán durante la funcion
             ApiResponse response = new ApiResponse();
             queueFunctions = new QueueFunctions(_db);
             clientFuntions = new ClientFuntions(_db);
             try
             {
+                // Creamos / Editamos el cliente solicitante del turno
                 ResponseIdExceptionFunction clientResponse = clientFuntions.CreateOrUpdate(client);
                 if (clientResponse.Integer > 0)
                 {
@@ -68,7 +71,36 @@ namespace InitiumTest.Controllers
             } catch (Exception e)
             {
                 // Guardamos la excepción encontrada en un archivo log
-                Log.LOG("CONTROLADOR: QueueFunctions, FUNCIÓN: TurnAsing(), PARÁMETROS: Client.Identification = " + client.ClientIdentification.ToString() + ", "+ client.ClientName.ToString() + " ERROR: " + e.Message);
+                Log.LOG("CONTROLADOR: QueueController, FUNCIÓN: GetTurn(), PARÁMETROS: Client.Identification = " + client.ClientIdentification.ToString() + ", "+ client.ClientName.ToString() + " ERROR: " + e.Message);
+                response.ErrorMessage = e.Message;
+            }
+
+            return Json(response);
+        }
+
+        /*
+            Function QueueProcessed
+            Contiene la función principal de procesar una cola (poner el atributo Queue.Proccessed = false)
+        */
+        [HttpDelete("[action]/{Id}")]
+        public IActionResult QueueProcessed(int Id)
+        {
+            // Cargamos las variables que nos servirán durante la funcion
+            ApiResponse response = new ApiResponse() { Success = true, ErrorMessage = "", Data = "OK" };
+            queueFunctions = new QueueFunctions(_db);
+            try
+            {
+                // Creamos / Editamos el cliente solicitante del turno
+                ResponseIdExceptionFunction queueResponse = queueFunctions.queueProcessed(Id);
+                if (queueResponse.Integer == 0) {
+                    response.Success = false;
+                    response.ErrorMessage = queueResponse.Exception;
+                }
+            }
+            catch (Exception e)
+            {
+                // Guardamos la excepción encontrada en un archivo log
+                Log.LOG("CONTROLADOR: QueueController, FUNCIÓN: QueueProcessed(), PARÁMETROS: Queue.Id = " + Id.ToString() + " ERROR: " + e.Message);
                 response.ErrorMessage = e.Message;
             }
 
--- Functions/QueueFunctions.cs
+++ Functions/QueueFunctions.cs
@@ -34,10 +34,11 @@ namespace InitiumTest.Functions
             // iniciamos el objeto a retornar
             ResponseIdExceptionFunction turnAsignResponse = new ResponseIdExceptionFunction();
             turnAsignResponse.Integer = 0;
+            DateTime now = DateTime.Now;
             Queue newTurn = new Queue();
             newTurn.ClientId = ClientId;
             newTurn.Processed = false;
-            DateTime now = DateTime.Now;
+            newTurn.CreatedAt = now;
             try
             {
                 // Seteamos la posición uno másde la cantidad registrada para las colas
@@ -94,9 +95,84 @@ namespace InitiumTest.Functions
             return turnAsignResponse;
         }
 
+        /*
+            Function activesQueues
+            Consulta las colas pendientes o activas, ordenadas por el tiempo de atención descendiente
+            @params db ApplicationDbContext para tener el vínculo con la BD y hacer la consulta
+            @return List<Queue> una lista (entera o vacía) de colas pendientes
+        */
         protected List<Queue> activesQueues(ApplicationDbContext db)
         {
             return db.Queue.Where(q => q.Processed == false).OrderByDescending(ob => ob.TurnAt).ToList();
+        }
+
+        /*
+            Function queueProcessed
+            Actualiza por medio del ID de la Tabal Queue, la cola como procesada
+            @params Id, llave principal de la tabla Queue
+            @return ResponseIdExceptionFunction con intero y excepción en caso de existir
+        */
+        public ResponseIdExceptionFunction queueProcessed(int Id)
+        {
+            // Reutilizamos el modelo de respuestas con entero, donde el valor 1 corresponde a OK, y el 0 a error
+            ResponseIdExceptionFunction response = new ResponseIdExceptionFunction() { Exception = "", Integer = 1 };
+            try
+            {
+                // Consultamos la cola por su ID
+                Queue queue = _db.Queue.Find(Id);
+                // Evaluamos (para prevenir fallos) que esa cola si exista, sino, reportamos como no encontrada
+                if (queue == null)
+                {
+                    response.Exception = "Cola no encontrada con el ID " + Id.ToString();
+                    response.Integer = 0;
+                } 
+                // Ya existente la editamos
+                else
+                {
+                    queue.Processed = false;
+                    _db.Update(queue);
+                    _db.SaveChanges();
+                }
+            }
+            // Capturamos la excepción
+            catch (Exception e)
+            {
+                // Guardamos la excepción encontrada en un archivo log
+                Log.LOG("CONTROLADOR: QueueFunctions, FUNCIÓN: queueProcessed(), PARÁMETROS: Queue.Id = " + Id.ToString() + " ERROR: " + e.Message);
+                response.Exception = e.Message;
+                response.Integer = 0;
+            }
+
+            return response;
+        }
+
+        /*
+            Function queueList
+            Consulta las lista de las últimas 2 pendientes, y un límite de 5 procesadas
+            @return ApiResponse con success false y da error o true si va bien, errormessage quien indica el error (en caso de que aplique), y la data en caso de ser exitoso
+        */
+        public ApiResponse queueList()
+        {
+            ApiResponse apiResponse = new ApiResponse() { Success = true, ErrorMessage = "" };
+            DateTime startDateTime = DateTime.Today;
+            DateTime endDateTime = DateTime.Today.AddDays(1).AddTicks(-1);
+            try
+            {
+                List<Queue> pendingQueues = _db.Queue.Where(q => q.Processed == false).OrderBy(ob => ob.Position).Take(2).ToList();
+                List<Queue> lastQueues = _db.Queue.Where(q => q.Processed == true && q.CreatedAt >= startDateTime && q.CreatedAt <= endDateTime).Take(5).ToList();
+                pendingQueues.AddRange(lastQueues);
+                apiResponse.Data = pendingQueues;
+            }
+            // Capturamos la excepción
+            catch (Exception e)
+            {
+                // Guardamos la excepción encontrada en un archivo log
+                Log.LOG("CONTROLADOR: QueueFunctions, FUNCIÓN: queueList(), ERROR: " + e.Message);
+                apiResponse.ErrorMessage = e.Message;
+                apiResponse.Success = false;
+            }
+
+            return apiResponse;
         }
     }
 }
